d6998b63b40e1945167c83454f6ec4e7a2b4d2fd
emilio barrera <barrera_emilio@yahoo.es>
2016-08-23 15:55:54 +0200

    product = ok

--- ECommerce/Models/Category.cs
+++ ECommerce/Models/Category.cs
@@ -15,7 +15,7 @@ namespace ECommerce.Models
         [Index("Category_CompanyId_Description_Index", 2, IsUnique = true)]//Indices compuestos:department
         [Required(ErrorMessage = "The field {0} is Required.")]
         //[Range(1, double.MaxValue, ErrorMessage = "You must Select a {0}")]   
-        [StringLength(50, ErrorMessage ="The field {0} must be maximun {1} and minimun {2} characters length", MinimumLength = 1)]
+        [StringLength(55, ErrorMessage ="The field {0} must be maximun {1} and minimun {2} characters length", MinimumLength = 1)]
         [Display(Name = "Category")]     
         public string Description { get; set; }
 
